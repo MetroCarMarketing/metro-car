814af03c2adb7986a5b37725ec1a85c06ca4244b
David Moller <davidmollera@hotmail.com>
2023-08-03 12:09:07 -0500

    made the app unscrollable when the burger menu is open

--- src/Components/Burger.jsx
+++ src/Components/Burger.jsx
@@ -5,48 +5,47 @@ import { useNavigate } from "react-router-dom";
 import { Home, MenuOpenOutlined } from "@mui/icons-material";
 
 const Burger = ({ translation }) => {
-  const navigateTo = useNavigate()
+  const navigateTo = useNavigate();
 
-  const navigateClose = ( value ) => {
-    navigateTo(value)
-    document.querySelector('body').classList.toggle('menuOpen')
-  }
+  const navigateClose = (value) => {
+    navigateTo(value);
+    document.querySelector("body").classList.remove("menuOpen");
+  };
 
   const closeMenu = () => {
-    document.querySelector('body').classList.toggle('menuOpen')
+    document.querySelector("body").classList.remove("menuOpen");
   };
 
   return (
-    <div className="burger ">
-      <MenuOpenOutlined onClick={closeMenu} />
-      <IconButton
-        className="burger__home--icon"
-        onClick={() => navigateTo("/")}
-      >
-        <Home />
-      </IconButton>
-      <Button onClick={() => navigateClose("/services")}>
-        {translation.navLink1}
-      </Button>
-      {/* <Button onClick={() => navigateTo("/services")}>
-        {translation.navLink1}
-      </Button> */}
+    <div onClick={closeMenu} className="burger ">
+      <div className="burger__half">
+        <MenuOpenOutlined onClick={closeMenu} />
+        <IconButton
+          className="burger__home--icon"
+          onClick={() => navigateClose("/")}
+        >
+          <Home />
+        </IconButton>
+        <Button onClick={() => navigateClose("/services")}>
+          {translation.navLink1}
+        </Button>
 
-      <Button onClick={() => navigateTo("/coverage")}>
-        {translation.navLink2}
-      </Button>
+        <Button onClick={() => navigateClose("/coverage")}>
+          {translation.navLink2}
+        </Button>
 
-      <Button onClick={() => navigateTo("/fleet")}>
-        {translation.navLink3}
-      </Button>
+        <Button onClick={() => navigateClose("/fleet")}>
+          {translation.navLink3}
+        </Button>
 
-      <Button onClick={() => navigateTo("/contactus")}>
-        {translation.navLink4}
-      </Button>
+        <Button onClick={() => navigateClose("/contactus")}>
+          {translation.navLink4}
+        </Button>
 
-      <Button onClick={() => navigateTo("/aboutus")}>
-        {translation.navLink5}
-      </Button>
+        <Button onClick={() => navigateClose("/aboutus")}>
+          {translation.navLink5}
+        </Button>
+      </div>
     </div>
   );
 };
