0f0c24af64033b2a6fdce0b8aa56be90c2a2b608
David Moller <davidmollera@hotmail.com>
2023-08-09 09:22:10 -0500

    made css changes to the nav bar

--- src/Components/CarThumbnailMap.jsx
+++ src/Components/CarThumbnailMap.jsx
@@ -40,17 +40,6 @@ const CarThumbnailMap = ({ item }) => {
         </>
       )}
     </div>
-    // <div className="thumbnail" onClick={() => navigateTo(`/fleet/${item.id}`)}>
-    //   <figure className="thumbnail__wrapper">
-    //     <img src={image} alt="" />
-    //   </figure>
-    //   <div className="thumbnail__info">
-    //     <h2 className="thumbnail__title">{item.name}</h2>
-    //     <h3 className="thumbnail__price number">
-    //       $ {item.price} <span>/ h</span>
-    //     </h3>
-    //   </div>
-    // </div>
   );
 };
 
