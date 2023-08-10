17efe7ebeec00bc67066c85eca82015d7426b8ec
David Moller <davidmollera@hotmail.com>
2023-08-10 09:44:36 -0500

    created coveragemap jsx and css filees

---
+++ src/Components/CoverageMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/CoverageMap.css'
+
+const CoverageMap = () => {
+    return (
+        <div className='coverage__map--wrapper'>
+            
+        </div>
+    );
+}
+
+export default CoverageMap;
