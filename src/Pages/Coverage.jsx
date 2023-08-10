22396f2d8a0e8d662e058c1381dc2e7d2dea409f
David Moller <davidmollera@hotmail.com>
2023-08-10 10:16:04 -0500

    used the top row class on the coverage row  and created a coverage css file

--- src/Pages/Coverage.jsx
+++ src/Pages/Coverage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import CarThumbnailMap from "../Components/CarThumbnailMap";
 import CoverageMap from "../Components/CoverageMap";
+import '../CSS/Coverage.css'
 
 const Coverage = ({ translation, cars }) => {
   return (
-    <div className="container top__container">
-      <div className="row">
+    <div className="container top__container coverage__container">
+      <div className="row top__row coverage__row">
         <CoverageMap/>
       </div>
     </div>
