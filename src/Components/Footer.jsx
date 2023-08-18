a0d426107793bd3ed4e32985225a174aaca3bce1
David Moller <davidmollera@hotmail.com>
2023-08-18 11:54:32 -0500

    added more code to the footer component and styled it in css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,8 +7,12 @@ const Footer = ({ translation }) => {
     <footer>
       <div className="row footer__row">
         <div className="footer__text--container">
+          <div className="footer__info--container">
+            <h1 className="footer__title">Quienes Somos</h1>
+            <p>MetroCar es una empresa prometedora Colombiana que se dedica a ofrecer un servico de lujo a sus pasajeros, los fundadores tienen una experiencia de 10 años en la industria y aunque aun es un empresa en crecimiento eso le permite darle la atencion y cuidado a todos sus clientes tal y como lo merecen. </p>
+          </div>
           <div className="footer__contact--container">
-            <h1>Informacion de Contacto</h1>
+            <h1 className="footer__title">Informacion de Contacto</h1>
             <ul className="footer__contact--list">
               <li className="footer__contact--item">
                 <Instagram/>
@@ -29,7 +33,7 @@ const Footer = ({ translation }) => {
             </ul>
           </div>
         </div>
-        <p className="footer__credits">{translation.footerCredits}<a className="title">A & D</a></p>
+        <p className="footer__credits">{translation.footerCredits}<a href='https://google.com' target="_blank" rel="noreferrer" className="title">A & D</a></p>
       </div>
     </footer>
   );
