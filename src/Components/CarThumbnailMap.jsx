e38888955fcc58f110c53e15ee221362b1283d46
David Moller <davidmollera@hotmail.com>
2023-08-23 12:08:34 -0500

    updated the carThumbnail map component to display # of passengers and bags

--- src/Components/CarThumbnailMap.jsx
+++ src/Components/CarThumbnailMap.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "../CSS/CarThumbnailMap.css";
 import { useNavigate } from "react-router-dom";
+import { Check } from "@mui/icons-material";
 
-const CarThumbnailMap = ({ item }) => {
+const CarThumbnailMap = ({ item, translation }) => {
   const navigateTo = useNavigate();
 
   const [img, setImg] = useState();
 
   useEffect(() => {
-    const image = new Image()
-    image.src = item.imageMain
+    const image = new Image();
+    image.src = item.imageMain;
     image.onload = () => {
       setTimeout(() => {
-        setImg(image)
-      },600)
-    }
-    
-  })
+        setImg(image);
+      }, 600);
+    };
+  });
 
   return (
     <div className="thumbnail" onClick={() => navigateTo(`/fleet/${item.id}`)}>
@@ -28,15 +28,27 @@ const CarThumbnailMap = ({ item }) => {
           <div className="thumbnail__info">
             <h2 className="thumbnail__title">{item.name}</h2>
             <h3 className="thumbnail__price number">
-              $ {item.price} <span>/ h</span>
+              $ {item.priceHour} <span>/ h</span>
             </h3>
+            <div className="thumbnail__organizer">
+              <h4>
+                pax: <span className="number">{item.pax}</span>
+              </h4>
+              <Check />
+            </div>
+            <div className="thumbnail__organizer">
+              <h4>
+                {translation.bags}: <span className="number">{item.bags}</span>
+              </h4>
+              <Check/>
+            </div>
           </div>
         </>
       ) : (
         <>
-        <div className="thumbnail__wrapper--skeleton skeleton"></div>
-        <div className="thumbnail__title--skeleton skeleton"></div>
-        <div className="thumbnail__price--skeleton skeleton"></div>
+          <div className="thumbnail__wrapper--skeleton skeleton"></div>
+          <div className="thumbnail__title--skeleton skeleton"></div>
+          <div className="thumbnail__price--skeleton skeleton"></div>
         </>
       )}
     </div>
