482f30390ce714901ab25130822183fd4231a0b1
David Moller <davidmollera@hotmail.com>
2023-08-24 09:58:46 -0500

    updated the pax and bags properties for all cars

--- src/Cars.js
+++ src/Cars.js
@@ -1,34 +1,5 @@
 export const cars = [
   {
-    id: 1,
-    name: "Prado",
-    nameEsp: "Prado",
-    type: "SUV",
-    typeEsp: "Camioneta",
-    brand: "Toyota",
-    description:
-      "The Toyota prado has 1 Diesel Engine and 1 Petrol Engine on offer. The Diesel engine is 4461 cc while the Petrol engine is 3956 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the prado has a mileage of 7.3 to 8.0 kmpl & Ground clearance of prado is 220 mm. The prado is a 8 seater 6 cylinder car and has length of 4,715 mm, width of 1,875 mm and a wheelbase of 2,790 mm.",
-    descriptionEsp:
-      "El Toyota Land Cruiser Prado cuenta con un fuerte carácter y un atractivo y robusto diseño basado en líneas agresivas y estilo compacto de gran elegancia con una mezcla de texturas de forma armónica para brindar un diseño que otorga distinción. Este potente todo terreno se ha reinventado y superado a sí mismo.",
-    model: 2019,
-    priceHour: 59.00,
-    priceDay: 477.00,
-    priceTrip: 90.00,
-    pax: 2,
-    bags: 4,
-    imageMain:
-      "https://www.toyota.com.co/wp-content/uploads/2020/08/PLATA-METALICO-14.jpg",
-    image2:
-      "https://www.toyotarp.com/images/ImagenesModelo/Prado/lineup/GT5L-8.webp",
-    image3:
-      "https://www.autosrodando.com/wp-content/uploads/2023/03/toyota-landcruiser-prado-2024.jpg.webp",
-    image4:
-      "https://i0.wp.com/minivanusa.com/wp-content/uploads/2022/07/2024-Toyota-Land-Cruiser-Prado-ernder.jpg?fit=640%2C335&ssl=1",
-    image5: "https://www.autopediame.com/storage/images/Toyota/Prado/exr40.jpg",
-    image6:
-      "https://yokomotor.com.co/toyota/wp-content/uploads/sites/2/2021/11/ROJO-3.jpg",
-  },
-  {
     id: 2,
     name: "CLA Coupe",
     nameEsp: "CLA Coupe",
@@ -38,11 +9,67 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 53.00,
-    priceDay: 428.00,
-    priceTrip: 80.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 53.0,
+    priceDay: 428.0,
+    priceTrip: 80.0,
+    pax: 3,
+    bags: 3,
+    imageMain:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
+    image2:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+    image3:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
+    image4:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+    image5:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+    image6:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+  },
+  {
+    id: 16,
+    name: "A6",
+    nameEsp: "A6",
+    type: "Luxury Sedan",
+    typeEsp: "Sedan Gama Alta",
+    brand: "Audi",
+    description: "description test",
+    descriptionEsp: "description espanol",
+    model: 2019,
+    priceHour: 53.0,
+    priceDay: 428.0,
+    priceTrip: 80.0,
+    pax: 3,
+    bags: 3,
+    imageMain:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
+    image2:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+    image3:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
+    image4:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+    image5:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+    image6:
+      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+  },
+  {
+    id: 17,
+    name: "Series 5",
+    nameEsp: "Series 5",
+    type: "Luxury Sedan",
+    typeEsp: "Sedan Gama Alta",
+    brand: "BMW",
+    description: "description test",
+    descriptionEsp: "description espanol",
+    model: 2019,
+    priceHour: 53.0,
+    priceDay: 428.0,
+    priceTrip: 80.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -66,11 +93,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 41.00,
-    priceDay: 328.00,
-    priceTrip: 61.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 41.0,
+    priceDay: 328.0,
+    priceTrip: 61.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -94,11 +121,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 41.00,
-    priceDay: 328.00,
-    priceTrip: 61.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 41.0,
+    priceDay: 328.0,
+    priceTrip: 61.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -122,11 +149,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 41.00,
-    priceDay: 328.00,
-    priceTrip: 61.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 41.0,
+    priceDay: 328.0,
+    priceTrip: 61.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -141,6 +168,35 @@ export const cars = [
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
   },
   {
+    id: 1,
+    name: "Prado",
+    nameEsp: "Prado",
+    type: "SUV",
+    typeEsp: "Camioneta Convencional",
+    brand: "Toyota",
+    description:
+      "The Toyota prado has 1 Diesel Engine and 1 Petrol Engine on offer. The Diesel engine is 4461 cc while the Petrol engine is 3956 cc . It is available with Automatic transmission.Depending upon the variant and fuel type the prado has a mileage of 7.3 to 8.0 kmpl & Ground clearance of prado is 220 mm. The prado is a 8 seater 6 cylinder car and has length of 4,715 mm, width of 1,875 mm and a wheelbase of 2,790 mm.",
+    descriptionEsp:
+      "El Toyota Land Cruiser Prado cuenta con un fuerte carácter y un atractivo y robusto diseño basado en líneas agresivas y estilo compacto de gran elegancia con una mezcla de texturas de forma armónica para brindar un diseño que otorga distinción. Este potente todo terreno se ha reinventado y superado a sí mismo.",
+    model: 2019,
+    priceHour: 59.0,
+    priceDay: 477.0,
+    priceTrip: 89.0,
+    pax: 5,
+    bags: 3,
+    imageMain:
+      "https://www.toyota.com.co/wp-content/uploads/2020/08/PLATA-METALICO-14.jpg",
+    image2:
+      "https://www.toyotarp.com/images/ImagenesModelo/Prado/lineup/GT5L-8.webp",
+    image3:
+      "https://www.autosrodando.com/wp-content/uploads/2023/03/toyota-landcruiser-prado-2024.jpg.webp",
+    image4:
+      "https://i0.wp.com/minivanusa.com/wp-content/uploads/2022/07/2024-Toyota-Land-Cruiser-Prado-ernder.jpg?fit=640%2C335&ssl=1",
+    image5: "https://www.autopediame.com/storage/images/Toyota/Prado/exr40.jpg",
+    image6:
+      "https://yokomotor.com.co/toyota/wp-content/uploads/sites/2/2021/11/ROJO-3.jpg",
+  },
+  {
     id: 6,
     name: "Expedition",
     nameEsp: "Expedition",
@@ -150,11 +206,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 59.00,
-    priceDay: 477.00,
-    priceTrip: 89.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 59.0,
+    priceDay: 477.0,
+    priceTrip: 89.0,
+    pax: 5,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -178,11 +234,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 59.00,
-    priceDay: 477.00,
-    priceTrip: 89.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 59.0,
+    priceDay: 477.0,
+    priceTrip: 89.0,
+    pax: 5,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -207,11 +263,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 64.00,
-    priceDay: 517.00,
-    priceTrip: 129.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 64.0,
+    priceDay: 517.0,
+    priceTrip: 129.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -236,23 +292,22 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 64.00,
-    priceDay: 517.00,
-    priceTrip: 129.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 64.0,
+    priceDay: 517.0,
+    priceTrip: 129.0,
+    pax: 3,
+    bags: 3,
     imageMain:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
+      "https://www.toyota.com.co/wp-content/uploads/2020/08/PLATA-METALICO-14.jpg",
     image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+      "https://www.toyotarp.com/images/ImagenesModelo/Prado/lineup/GT5L-8.webp",
     image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
+      "https://www.autosrodando.com/wp-content/uploads/2023/03/toyota-landcruiser-prado-2024.jpg.webp",
     image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+      "https://i0.wp.com/minivanusa.com/wp-content/uploads/2022/07/2024-Toyota-Land-Cruiser-Prado-ernder.jpg?fit=640%2C335&ssl=1",
+    image5: "https://www.autopediame.com/storage/images/Toyota/Prado/exr40.jpg",
     image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+      "https://yokomotor.com.co/toyota/wp-content/uploads/sites/2/2021/11/ROJO-3.jpg",
   },
   {
     id: 10,
@@ -265,11 +320,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 64.00,
-    priceDay: 517.00,
-    priceTrip: 129.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 64.0,
+    priceDay: 517.0,
+    priceTrip: 129.0,
+    pax: 3,
+    bags: 3,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -293,11 +348,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 51.00,
-    priceDay: 408.00,
-    priceTrip: 76.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 51.0,
+    priceDay: 408.0,
+    priceTrip: 76.0,
+    pax: 6,
+    bags: 6,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -321,11 +376,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 51.00,
-    priceDay: 408.00,
-    priceTrip: 76.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 51.0,
+    priceDay: 408.0,
+    priceTrip: 76.0,
+    pax: 6,
+    bags: 6,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -349,11 +404,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 59.00,
-    priceDay: 477.00,
-    priceTrip: 89.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 59.0,
+    priceDay: 477.0,
+    priceTrip: 89.0,
+    pax: 15,
+    bags: 15,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -377,11 +432,11 @@ export const cars = [
     description: "description test",
     descriptionEsp: "description espanol",
     model: 2019,
-    priceHour: 59.00,
-    priceDay: 477.00,
-    priceTrip: 89.00,
-    pax: 2,
-    bags: 4,
+    priceHour: 59.0,
+    priceDay: 477.0,
+    priceTrip: 89.0,
+    pax: 15,
+    bags: 15,
     imageMain:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
     image2:
@@ -395,88 +450,32 @@ export const cars = [
     image6:
       "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
   },
-  {
-    id: 15,
-    name: "Hino",
-    nameEsp: "Hino",
-    type: "Coach",
-    typeEsp: "Bus",
-    brand: "Mercedes",
-    description: "description test",
-    descriptionEsp: "description espanol",
-    model: 2019,
-    priceHour: '???',
-    priceDay: '???',
-    priceTrip: "???",
-    pax: 2,
-    bags: 4,
-    imageMain:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
-    image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
-    image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
-  },
-  {
-    id: 16,
-    name: "A6",
-    nameEsp: "A6",
-    type: "Luxury Sedan",
-    typeEsp: "Sedan Gama Alta",
-    brand: "Audi",
-    description: "description test",
-    descriptionEsp: "description espanol",
-    model: 2019,
-    priceHour: 53.00,
-    priceDay: 428.00,
-    priceTrip: 80.00,
-    pax: 2,
-    bags: 4,
-    imageMain:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
-    image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
-    image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
-  },
-  {
-    id: 17,
-    name: "Series 5",
-    nameEsp: "Series 5",
-    type: "Luxury Sedan",
-    typeEsp: "Sedan Gama Alta",
-    brand: "BMW",
-    description: "description test",
-    descriptionEsp: "description espanol",
-    model: 2019,
-    priceHour: 53.00,
-    priceDay: 428.00,
-    priceTrip: 80.00,
-    pax: 2,
-    bags: 4,
-    imageMain:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
-    image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
-    image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
-  },
+  // {
+  //   id: 15,
+  //   name: "Hino",
+  //   nameEsp: "Hino",
+  //   type: "Coach",
+  //   typeEsp: "Bus",
+  //   brand: "Mercedes",
+  //   description: "description test",
+  //   descriptionEsp: "description espanol",
+  //   model: 2019,
+  //   priceHour: "???",
+  //   priceDay: "???",
+  //   priceTrip: "???",
+  //   pax: 2,
+  //   bags: 4,
+  //   imageMain:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-01-DR.jpg",
+  //   image2:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+  //   image3:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
+  //   image4:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+  //   image5:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+  //   image6:
+  //     "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+  // },
 ];
