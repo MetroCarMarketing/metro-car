3aae2c627082a012273eca7799179da425b8a607
David Moller <davidmollera@hotmail.com>
2023-08-29 10:38:33 -0500

    created car highlights component and css file and imported it to the home page

---
+++ src/Components/CarHighlights.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../CSS/CarHighlights.css'
+
+const CarHighlights = () => {
+    return (
+        <section id="carHighlights">
+            
+        </section>
+    );
+}
+
+export default CarHighlights;
+
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Landing from "../Components/Landing";
+import CarHighlights from "../Components/CarHighlights";
 
-const Home = ({ translation, setActive }) => {
-  return <Landing translation={translation} setActive={setActive} />;
+const Home = ({ translation }) => {
+  return (
+    <>
+      <Landing translation={translation} />
+      <CarHighlights translation={translation} />
+    </>
+  );
 };
 
 export default Home;
