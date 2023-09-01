bd5e3b6e499443338f9f3077326165165ac767e3
David Moller <davidmollera@hotmail.com>
2023-09-01 10:35:34 -0500

    updates the images for the mid size sedans

--- src/Cars.js
+++ src/Cars.js
@@ -46,7 +46,7 @@ export const cars = [
     pax: 3,
     bags: 3,
     imageMain:
-      "https://images.91wheels.com/assets/b_images/main/models/profile/profile1661236986.jpg",
+      "https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAI1Y9zeXb9y_bLmszGyyV0iUUUbGR8jK3qKPvTKTvWUme1VmRh9EiIyUlTJCZlHJpkLZnuv7nOcPeO4f3ufc17nOfe7rfb_W-67eAES-3oDA3FxZ85iJHouduYwPQIAnAAAPreObK1dvM-4Rk96a_79l6iBA6O9g54mHdqjhkzm52WIdxP5boDg9PaXCI7OVFnUI8BH19rxEgnY0Q7zQMFne9li_YOFyq6LnskW8f7bOASjD9FS4hPP2_Vy8Vur85HuWRr4zlQAWXBMg6VyX9a23mOqbrhn4dc9e6C-AWQl6f6lbnl9cZDhIxXHM6kuwNyUByEvzxX3vtlaAjVhHMKXPoog0RQwEUFj019koDaGEG0853p30V5Ay9vX-BHAbN9ioNhoV5t2Kec_PkLP8-lLMFQADBO-Vphrd_n1_0enT1orO9fB212QAo2ubLE7Zz5PQ9U6epVQylFTemhkEEP9gofXCgfXCzpfBcZsPZFrtwuTsANZYO4rrsU-SZQscfz85oT553-FnBOATojbe7IaDif1t58r2scTH83qrzQAanq4Nlce6MOfQsVNi2KqkTvW97wLoM0pP8b32b69k95OmMJFucgoq1hEAW5mNYycke-McIt2G5CQqmH0vGHUCWDkaRhojqJxgUkDo9uf7skl2sO8GgNoTQdac2hN91s9mredJxvn1e9NlANR9FSKymiK0Zyau5HIoz4mPz6XXDuBgF_3VmiLH9Ig_3kaePjDN1ZbxHoCZk-2ay0IRAwre6m-wRs5t8xN7LAAqsVLGc3HpHNxPeDy8T1t0nHMwJw6gHkfw2LXm0uQyQjrX2YuLZnqJ9EMAhhINHIgch30YWPirmVH1jXB3WeYagNJh_u9LsW64S52HzK52xdE61FnlAKqtj6h4EB-rq0eajLaRUeHbFLAEAUimGNUoZRQqyvb0we3gkLqjFFyhB4DBI_LYAtmtnuEGhdHodHOSNIP2mwAeqYyHFvGnqmA1ps5xBwjWqvEZo-9mWyR5OcBcEs8zkkJoVZGI06yEcBdAPgJMhfjoZ4W04rsOX_IZVewE1VAnDTEJX2eLPOWjo3gsMw-p8fQu1g4DWMpF6J5Tm8pifqBAICYhMUfTnHUK4BUJPCOH3yscEV_Emq45FF496FJ2AdBmh7-C2_yUcQDLi9NTIAuViBJOBBDWjYn25Q7vB8dMtdTL02gEmlCiTtYw71VvCmmqkBasMwVd9CkVXZNVADDbP_4-h9KCFGZgY2Bb5mtJKyldCYB-GPMX8_GVlP5_BjVDP8TwXB29tgkge6fTHMe7bt8Gsre3FGWdjSxnzH8ASFfjm2kYx5258-d55PZ-Rtxr1SaEqAnT_UslTmw8JiKOlyd_0Sj4-P_sB5BKS3XGoIrR2Hj5qss5xUz_CHulhwBOeXayT6TYm2Ve6RhIUpjTO2GiQc-tl3pgwm64OkpflxA8LLoj3pgbSgxg53m_a2prKqnX3l6_y_M5Mc1pCD8A7W1l8S2MHZDcPEcTeVynWm8R5oz4xmN4klDCH0YQIljcafrQO8tKfxTtbfgWMj-XXFCuyfXvuOXhY9vsmapSAFtKmwVGZ9mWBArnuW5S4-H-TVslAGhMwtf1SS2vKlgoyaO8MHAuTIlbFEDNdZX6F8HfFn-GJLpVXOpo2DaldUU9Y6-4bFeGaY492XqeFBlDXn-VcRlx3lyq6GQD-E5rV8xbWPLcrvPYKADQfUFIQ-2e_7LDPsdOST3b0OFK6QqALIXD63_CYx-K0_nT-BhHUYaNS7wH8EMYf9ZHA2MRJ7zDSnmixV2JvA1vAK8ljng7JhSTdJMqCtQbxctQX1-yBtD5tnGU98DB3ZKQz9kdQnebcQe3OAHUmWeqJVEN2v2h7VrN9aHszlW3QoS-d0-_rpOm75WrZb-xVjX0pS36s404VO5RlkFEyAB09HFZbjKuuCsKRjgAJ-1PRW2krs9CAr9EPL7lECMiyWAAf_d0TP7DfCZf4pI6UWqTcb-owrMAYGHjww8MrYRztQ_EAotpeoh3JDmkAMx9v5eia_nVZS06VJys2tFCOeOfOoAKivl26VTHnnz4_C6vw7BLLHqs-QDmq_DxZNXL4sbZN2ZscugvDg-nNSHOD1wlZCsMHY4TFGFa264cNMK6WwHoVsXtkyNWUzAzbEHnXeISORnYiLTvDjFfu6DUs_6JXJHMHEwopTKnMwOAT7fw2OOqBXk4IiVCQuN3Ia_SdUYAT8TUQy3Pi1Yy7EzTypcpP06jzJ0DsCOA6ffwpm28rlOF1FKZ5AqMGItGKnf45UJn58r0mb4s1Z1k5ZfFomuI8xGpbOTB0WJZ8Vi3z3UB2RRBlB6GCH1w6EkV7buaO_4G0ywUMrm-YXkhAJ5trRuY01xwUD3u1depmxVgGD06AtD13aSX46P0VfWKwLkpMnOpwVUzxJZ_Fa5TwiXv9RaDM-Ll29OCj7I6U1B3TvWSQ9XVNVrMGUZYldYeVXdEswEoanKULd_VQ8xEyMzTxKdUH_SURA1Aa22Fj-6tGKFaO5_eD3mbtCL3n4gBOO_cwNku9dDh0CBcokv3VMt_Ihyhr1ro8Ly3LrNy25Suwi-2TPI6kQsVALalHU1d0THyC0psrCJkKh924iN6DiCB-HfPP5aEaxt0tirnzy1Z0p4sEwDoOBSglljKLh_-_NUzi0M56tpSN08A_Z12e_MvUZxdd2ViOQ7l2g8J_oYFkOi-NzP5da6B9Ovdye8FjZZeyQnSAXjo8kbY7FpyRIEg_e6jQSENyZasCaSTXmJ9pQL567HdH7X8iBPdS2_f0QdQkItr5aH-km_xg68xgrd5ZMqTv38CcKPJwT6ceC-hovsyC8_dB86M7768BbDYiiMT86tjJl4pvOsv4wsd-cp4FaRn158wsEjxyTZd8T41VVvVvLYg5IA4ZOjOgsuj4GprVEh39C7MnR6kCAUQa3932nw3Aud-Xj6sTOFNE9_DXDcA7Xd-i7z1qqE89qL-9aHcrn-NSht5i9wCT3we4Lu40EvIeGLGEtys4sID4PQeAe_rT8Lk35R3TNaLIzNcKFyQ40w8VNtmnJRzUlkymCd1t9ezSsdLBXD3JoXYp8_2VTa4wMsRDx7tTvzKswNwhv-FsH-uYcXXh6U5lo9ZPxOW-cUC2KQVlGbWKfpgfjm35CvnHCut6Txykd6Hmn1SJlQH-gcCVwZpn-nbv-pBeLAkk8U06PYkF-Ve12RT0IinvsXthJLCDG7yfVSF2t1LcnpfKLx4mea_OgO4Uj37XPixZ4OD7ESPbhEzz9ukpyitqIp-Hvwh06tdNb5FG_UXtLlPGj5BLn1z9lHXo1H8iTdxb3WYyD1uhstWAci_EkM-UDFmcrV11_ZjQTD7bFgGUo0Gc6Uz3KZDbcG_ezx_k98YITPvRT4Uen-2oli-LeX1Oa3pcVGx7U8WPXyIx0znW665dj44l2b1uKTqHO-K76YmgJyxtIORGhiNPiku_rwO7Yl42Ux8lB_Cl9Y8t1NrlwR7XlK6sK8IKj7lQqjuxsRXSO-MbMbrHnMfnnXvaKRCCe9J5bBKXxBTVKJ7Bl5I2rre8O7WHwC7K2QCpDwscpUW40MH-w3eZg5kkwLInAvKVbTPO2AV1TbeZdwJubX7ehrAi97tdszN_dM4sT7TBiEXAnb7GOQifeZd1tNxMKnQfveJ9qoYfX4_oy-ACf5sU6k5sUw6SwvkCrF2_kdySXcAjH89l5V77meNTQ-veZe7suuc1AFyvZvW-ZtLUSGz9aYerceBCtVWHz8jZg21M-a4mHbRfHMsZ5D1oFP9xRl2iDyrtXCez60yHJsS3kLmGbmn-r4KJTHudysu9fTGyRp586KCvJlhepkli0j7aC_450_ahIb8YHI6j33gxcuxHIn6II1v4FzMPSTFxCDdQyhSQyPE-xLANNPuIsHKCyeTIkTQa0_BeVb2BPGizG8O1z-2-HZ2LFAV26y1_iv2bR6AY2RRel2zepzlAd_D9in8JIZqFFDm-uu1spPpxppppDkx-lHi1qZEajA6mxn298_klK8-c-rpYsOujJor_OwoVUQatgz0HVs_z3j5Nyn-6nxmWuv9E_QOGpbvDSzpmBnvsk0UXuBvkvzhjjj0B7NqYuFrT_vNcm2NPvw01euLEnI9Zkqbe2sd5UklZ3yusPgIBAla8VAjlOR1PzWOviwtq9776hOVF3l_BT7KBHgUWi_Eint-DDq-u1nzidk2huYe0p2FnSEOAXH6x5L6XREhzCv6DFfeoFOsWs0HD4u91FoiYFDb9Iq72Dhmpog4TyKuWUuY85Lddi9sNJML1y8mjlCi2PzzXfWLZFqBeyNOsTgmYrm2fgOUlUklmpXwWsbGjzeWDcrznL12YpFGNZAkj2R03QqSPqLPJHASEPUxs6YHEFgl6Qp1TS08tXz1LwHbPpriEFEDYMr2jPsY9sznwJjTQGl-jw8GPPmILRd5aMOM_SnvKK7fypSVE7ZhZa2TQLmEY9OgOBBPdIKr7n4AoCJN9niEFPzo3dOBGZ6YgPfR9H3P2myORp540ACYvBJQGzfXl17F6WxrguGs4vRcegZgVIRkeJgMpmOzlvuNsbFjxKxOPZoZ4sYcqcep3AP_aWD5bsSslNMdESJ32jgnVB_ot6o1d3-ynUtyKarvGRVK_BlDrZ54u8X_FlYKgOpln72PkcdIPckydTdwwua5ZdMNOp4-k90B6kkoIxIY4E5uPlObW2-Tz9i6KpySgZFHaqR8K0A2B0cl9mt5wDIYs3ju-7RcOpqS6rn3ZpNquYfK968bUZyX4SiLfoW-pnP7Mncd4eFL331Xr-HLl2JeyaBkftuI7vbibwaNsk26C1JX2sAhxQvk_tnHzwOIJ_8V7FffW6Y9qYkia7qO9Ew2wzQ0WuMGZa5ac2PvhVOnBaJS1DMDgohmw_Eml0fbbmJmWRI7X5OyUcIb0Z6VosspFh5QadrgYk_haB0nf41QbW0pxPCv5_k3obxC_IINa96Z5kuo6wRtrmqbMh6FXDh_hxund3ouPUBYb1yQ43So8E7QTF-zn6RgiRSwxBYDWOf90oT7hUbfWdOZH8Yc02wVyuqFKLX1DYCxpHnZURs7luOW70_SxhMQzgISDqk_SDZydScsJkm_tT7zU-qZHEqORjf0WY1aOcykVT_aqf8g69K1lkVsaZTewnD_9M2mGKfQuofhY7SdQn4cFd_I6Fy52N33m9HPbmCNxq06dB75W_0v4eASU9vVjSrutAyqSDva72fQzECbSayrTcOASXq2wViUVMZ6OmCLJqpmzs1t463sfh6JmxlS3Bb7OqzyyE0t8C2z9tjBTn5z7kujNnZ3In00-dy4Jfb9rVtzmxD_Rfv64u7kj72CSM_Cxqhzkhlqzdz5iurn67D6W75nMMhbXrXLmY-V1q7U7keN84hnx6USoAlb0WDkc-OUsr4ZtcK9JTu_jjGDO92ACM39-COoECyjQsGICmkc-N_63x8BSlTwEv-7J44mAOgZAJCQgv8uSlpfb1dPW29btwveDnc9PdzvOvk5nBUQ_39etIRKRhq3_gdCWKPArhAAAA?wid=1142",
     image2:
       "https://car-images.bauersecure.com/wp-images/12551/audi_a6_2018_12.jpg",
     image3:
@@ -104,17 +104,17 @@ export const cars = [
     pax: 3,
     bags: 3,
     imageMain:
-      "https://www.chevrolet.com.ar/content/dam/chevrolet/mercosur/argentina/espanol/index/cars/cruze-5-premier/colorizer-1/02-images-2021/colorizer-azul-eclipse.jpg?imwidth=960",
+      "https://www.chevrolet.com.ar/content/dam/chevrolet/mercosur/argentina/espanol/index/cars/2019-cruze-premier/colorizer/enero-21/colorizer-preto-ouro-negro.jpg?imwidth=600",
     image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/2017_Chevrolet_Cruze_%28interior%29.jpg/1280px-2017_Chevrolet_Cruze_%28interior%29.jpg",
     image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+      "https://www.skywayautogroup.com/assets/stock/expanded/transparent/640/2017chc300002_640/2017chc300002_640_28.png?bg-color=FFFFFF&width=400%20400w",
+    // image4:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+    // image5:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+    // image6:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
   },
   {
     id: 4,
@@ -133,17 +133,17 @@ export const cars = [
     pax: 3,
     bags: 3,
     imageMain:
-      "https://credivehiculos.com/images/stories/fruit/Hyundai%20i35.png",
+      "https://credivehiculos.com/wp-content/uploads/2014/02/hyundai-i35-coupe.png",
     image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+      "https://www.chinamobil.ru/photo/-import/hyundai/hyundai-ix35/folder1/full/hyundai-ix35-int01.jpg",
     image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+      "https://cdn2.buyacar.co.uk/sites/buyacar/files/styles/w860/public/hyundai-ix35-rear-seats13.png?itok=fI4JQ0sY",
+    // image4:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+    // image5:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+    // image6:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
   },
   {
     id: 5,
@@ -162,17 +162,17 @@ export const cars = [
     pax: 3,
     bags: 3,
     imageMain:
-      "https://www.mazdausa.com/siteassets/vehicles/2023/mazda3-sedan/build-and-price/trims/34-jellies/carbon/2022-m3s-fwd-2-5s-carbon-edition-polymetal-gray.png",
+      "https://www.dealerfireblog.com/vicbaileymazda/wp-content/uploads/sites/894/2020/10/2021-Mazda3-Sedan-Jet-Black-Mica_o.jpg",
     image2:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/class-page/series/2023-CLA-COUPE-CT-1-3-02-DR.jpg",
+      "https://crdms.images.consumerreports.org/c_lfill,w_720,q_auto,f_auto/prod/cars/chrome/white/2015MAZ004b_1280_12",
     image3:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-010-A-FE-DR.jpg",
-    image4:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
-    image5:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
-    image6:
-      "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
+      "https://cdcssl.ibsrv.net/autodata/images/?IMG=USC90MAC171D021028.JPG",
+    // image4:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-017-C-FI-DR.jpg",
+    // image5:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-018-C-FI-DR.jpg",
+    // image6:
+    //   "https://www.mbusa.com/content/dam/mb-nafta/us/myco/my23/cla/gallery/series/gallery-class/2023-CLA-COUPE-GAL-012-J-FI-DR.jpg",
   },
   {
     id: 1,
